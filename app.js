2914e53e1303b950792305e8d572769d0db21e76
Carlos Gomez <carlosgomez_87@hotmail.com>
2023-01-25 13:55:43 -0300

    cambios agregando function

--- app.js
+++ app.js
@@ -6,27 +6,27 @@ const tacosTutaco = [
     {tacos: "Taco Tour", proteina: "Todas", porción: "350g", picante: "Sin picante", precio: 4500 }
 
 ]
-
 const shopcart = []
 
-let pedido = prompt("¿Quieres elegir uno de nuestros Tacos? (Orale que sí) o (Chanfles que paso, no)")
+let pedido = prompt("¿Quieres elegir uno de nuestros Tacos? (Orale que si) o (Chanfles que paso,no)")
 
-while (pedido != "Orale que sí" && pedido != "Chanfles que paso, no") {
-    alert("Responda por Orale que sí o Chanfles que paso, no. Muchas Gracias")
-    pedido = prompt("¿Quieres elegir uno de nuestros Tacos? (Orale que sí) o (Chanfles que paso, no)")
+pedidos(pedido,tacosTutaco)
+
+function pedidos(pedido,tacosTutaco) {
+    
+    if(pedido == "Chanfles que paso,no"){
+     return alert("Muchas gracias, nos vemos la proxima");
+    }
+    
+    else if (pedido == "Orale que si"){
+        let todosLosTacos = tacosTutaco.map((tuTaco) => tuTaco.tacos + " " + tuTaco.precio + "$");
+     alert(todosLosTacos.toString())
+    
+    }
+    else alert("Selecciona una orden valida, carnal ");
 }
 
-if (pedido == "Orale que sí") {
-    alert("Te mostramos todos los tacos de Tutaco")
-    let todosLosTacos = tacosTutaco.map(
-        (tuTaco) => tuTaco.tacos + " " + tuTaco.precio + "$"
-    );
-    alert(todosLosTacos.join(" - "))
-}else if (pedido == "Chanfles que paso, no") {
-    alert("Hasta la próxima, te estaremos esperando!!!")
-}
-
-while (pedido != "Chanfles que paso, no") {
+while (pedido != "Chanfles que paso,no") {
     let tuTaco = prompt("Agrega o elige un taco a tu carrito de tacos")
 
     let tacoEncontrado = tacosTutaco.find((item) => item.tacos == tuTaco)
@@ -39,9 +39,9 @@ while (pedido != "Chanfles que paso, no") {
         alert("Disculpa, pero no tenemos ese taco")
     }
 
-    pedido = prompt("¿Quieres seguir comprando? Orale que sí / Chanfles que paso, no")
+    pedido = prompt("¿Quieres seguir comprando? Orale que si / Chanfles que paso,no")
 
-    if (pedido === "Chanfles que paso, no") {
+    if (pedido === "Chanfles que paso,no") {
         alert("Gracias por tu compra: Te estaremos esperando de vuelta, para otro Taqueteo en Tu Taco.com")
         shopcart.forEach((comprafinal) => {
         console.log(`tuTaco: ${comprafinal.nombre}, unidades ${comprafinal.cantidad}, total a pagar por tacos ${comprafinal.cantidad * comprafinal.precio}`)
@@ -52,3 +52,4 @@ while (pedido != "Chanfles que paso, no") {
 
 const total = shopcart.reduce((acc, el) => acc + el.precio * el.cantidad, 0)
 console.log(`El total a pagar por la compra del taco o los tacos es:  ${total}`)
+
